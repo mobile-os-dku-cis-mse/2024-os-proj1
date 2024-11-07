2827823d8385a8fd0a902f73ecd1eee56c5d2893
Kcits970 <geometricstick@gmail.com>
2024-11-07 23:45:54 +0900

    reduced verbose code

--- main.c
+++ main.c
@@ -5,20 +5,33 @@
 #include <sys/types.h>
 #include <sys/time.h>
 #include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include "pidq.h"
 
 int ticks;
+int msqid;
+pidq running_q;
+pidq waiting_q;
+
+struct msgbuf
+{
+	long mtype;
+	char buf[128];
+};
+
+void run()
+{
+	// get message from parent.
+}
 
 void alarm_handler(int)
 {
 	ticks++;
-
-	// printf is non re-entrant; should not be called in a signal handler.
-	printf("timer invoked\n");
 }
 
-int main()
+static void enable_ticks()
 {
-	// itimer setup.
 	struct itimerval timer = {{1, 0}, {1, 0}};
 	setitimer(ITIMER_REAL, &timer, NULL);
 
@@ -26,42 +39,57 @@ int main()
 	memset(&sa, 0, sizeof(sa));
 	sa.sa_handler = alarm_handler;
 	sigaction(SIGALRM, &sa, NULL);
+}
 
-	pid_t children[10];
-	pid_t ppid = getpid();
+static void disable_ticks()
+{
+	struct itimerval ntimer = {{0, 0}, {0, 0}};
+	setitimer(ITIMER_REAL, &ntimer, NULL);
+}
 
+void schedule()
+{
+	puts("scheduler invoked!");
+	/*
+	1. if child has finished job, pop and push to waiting queue.
+	2. if child has used all ticks, pop and push to back.
+	3. if queue is not empty, signal the child at front.
+	4. decrease io time for every process in waiting queue.
+	5. if some processes have completed their io jobs, push them back to the running queue.
+	*/
+}
+
+int main()
+{
+	msqid = msgget(IPC_PRIVATE, IPC_CREAT | 0666);
+	pidq_init(&running_q, 10);
+	pidq_init(&waiting_q, 10);
+
+	// spawning 10 child processes.
 	for (int i = 0; i < 10; i++)
 	{
 		pid_t pid = fork();
 
-		if (pid < 0)
-		{
-			// kill all children.
-			// ...
-			exit(EXIT_FAILURE);
-		}
-
 		if (pid)
-		{
-			children[i] = pid;
-			continue;
-		}
+			pidq_push(&running_q, pid);
 		else
 		{
-			printf("child process[%08X]\n", getpid());
-			exit(EXIT_SUCCESS);
+			run();
+			exit(0);
 		}
 	}
 
-	// wait until every child process terminates.
-	while (wait(NULL) > 0);
+	enable_ticks();
 
 	while (ticks < 10)
-		sleep(1);
-	
-	struct itimerval ntimer = {{0, 0}, {0, 0}};
-	setitimer(ITIMER_REAL, &ntimer, NULL);
+	{
+		pause();
+		schedule();
+	}
 
-	printf("parent process[%08X]\n", getpid());
-	return 0;
+	disable_ticks();
+	
+	pidq_destroy(&running_q);
+	pidq_destroy(&waiting_q);
+	exit(0);
 }
