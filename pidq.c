b33aebad410b8c51b566a026f5050643b2f27efa
Kcits970 <geometricstick@gmail.com>
2024-11-08 23:30:24 +0900

    fixed pidq implementation

--- pidq.c
+++ pidq.c
@@ -23,22 +23,20 @@ int pidq_empty(pidq *q)
 
 void pidq_push(pidq *q, pid_t val)
 {
-	q->mem[(++q->back) % q->cap] = val;
+	q->mem[q->back = (q->back+1) % q->cap] = val;
 	q->sz++;
 }
 
 pid_t pidq_peek(pidq *q)
 {
-	return q->mem[0];
+	return q->mem[q->front];
 }
 
 pid_t pidq_pop(pidq *q)
 {
+	pid_t pid = q->mem[q->front];
+	q->front = (q->front+1) % q->cap;
 	q->sz--;
-	return q->mem[(q->front++) % q->cap];
-}
-
-pid_t pidq_at(pidq *q, int off)
-{
-	return q->mem[(q->front + off) % q->cap];
+	
+	return pid;
 }
