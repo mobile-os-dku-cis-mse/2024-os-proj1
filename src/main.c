a7589ef904476c095475ba5f9dec93a04f978eb2
HOchacha <outcider112@dankook.ac.kr>
2024-11-13 15:35:18 +0900

    add result

--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@ int main(int argc, char * arg[])
 {
 	int n_process = 10;
 	pcb_queue* ready_queue_o = malloc(sizeof(pcb_queue) * n_process);
-	IO_result = fopen("IO_result.txt", "w");
+	IO_result = freopen("scheduler_dump.txt", "w", stdout);
 	launch_scheduler_and_worker(ready_queue_o, n_process);
 
 	scheduler_run(ready_queue_o, 10);
