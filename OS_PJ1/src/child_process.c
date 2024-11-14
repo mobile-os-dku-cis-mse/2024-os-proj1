568f3100af658c88ce99dda970260d5c4f9b204e
yunseoLee <lys139011@gmail.com>
2024-11-14 11:16:53 +0900

    Squashed Commits

--- OS_PJ1/src/child_process.c
+++ OS_PJ1/src/child_process.c
@@ -5,20 +5,7 @@
 #include <time.h>
 #include "child_process.h"
 #include "message_queue.h"
-
-#define NUM_CHILDREN 10
-#define TIME_QUANTUM 1
-
-#define NUM_CHILDREN 10
-#define TIME_QUANTUM 1
-
-#define NUM_CHILDREN 10
-#define TIME_QUANTUM 1
-
-extern int msgid;
-extern int remaining_quantum[NUM_CHILDREN];
-extern pid_t child_pids[NUM_CHILDREN];
-extern int current_child;
+#include "scheduler.h"
 
 void create_children(int num_children, int msgid)
 {
